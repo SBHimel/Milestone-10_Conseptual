095e407323dbac76ea4af1d25c1f81a398c52f48
SBHimel <s.b.himel21@gmail.com>
2026-06-18 20:27:56 +0600

    Fixed embedded git repository issue

---
+++ tech-bazaar-starter-client/src/app/api/subsription/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+import { headers } from 'next/headers'
+import { stripe } from '@/lib/stripe';
+import { auth } from '@/lib/auth';
+
+
+
+export async function POST() {
+  try {
+    const headersList = await headers()
+    const origin = headersList.get('origin')
+
+    const userSession = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    const PRICE_ID = 'prod_Uj8sqcAVZamqsU'
+
+    
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: PRICE_ID,
+          quantity: 1,
+        },
+      ],
+      mode: 'subscription',
+      success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+    });
+    return NextResponse.redirect(session.url, 303)
+  } catch (err) {
+    return NextResponse.json(
+      { error: err.message },
+      { status: err.statusCode || 500 }
+    )
+  }
+}
+
+
+
+
+export async function GET() {
+    return NextResponse.json({message: "hello from subscription api route"})
+}
